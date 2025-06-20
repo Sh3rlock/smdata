9dcb9106facdf6dd7407a1a6483873be612e3580
Sh3rlock <matyass91@gmail.com>
2025-06-20 11:20:44 +0300

    smdata init

---
+++ smdataapp/urls.py
@@ -0,0 +1,10 @@
+# myapp/urls.py
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
---
+++ smdataapp/views.py
@@ -0,0 +1,5 @@
+# myapp/views.py
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, 'index.html' )
